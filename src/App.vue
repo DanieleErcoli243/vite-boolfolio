31722fe4a3bcb5613b8a821d1ff1f3e95ed3d7cb
Polvere243 <danielercoli@gmail.com>
2024-04-03 21:33:49 +0200

    imported axios in App and created a folder scss to create the style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script>
+import axios from 'axios'
 
+export default {
+    name: 'Boolfolio'
+};
 </script>
 
 <template>
 
 </template>
 
-<style></style>
+<style lang="scss">
+@use "./assets/scss/style.scss";
+</style>
