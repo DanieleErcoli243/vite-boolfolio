522e87deb91ac0aa29fb3d5bbffd7d1a15d26379
Polvere243 <danielercoli@gmail.com>
2024-04-04 01:18:36 +0200

    printed the projects in page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <script>
 import axios from 'axios'
 import AppHeader from './components/AppHeader.vue'
-
+const baseUri = 'http://localhost:8000/api/projects/';
 export default {
     name: 'Boolfolio',
     components: {
         AppHeader
     },
+    data: () => ({
+        projects: []
+    }),
     methods: {
-        fetchProjects() {
+        async fetchProjects() {
             try {
+                const { data } = await axios.get(baseUri);
+                console.log(data);
 
+                this.projects = data;
+            } catch (err) {
+                console.error(err);
             }
-            catch {
 
-            }
+
         }
+    },
+    created() {
+        this.fetchProjects();
     }
 };
 </script>
@@ -23,7 +33,13 @@ export default {
 <template>
     <AppHeader />
     <div class="container">
-
+        <div v-for="project in projects" :key="project.id" class="card">
+            <h2>{{ project.title }}</h2>
+            <figure>
+                <img src="" alt="{{project.title}}">
+            </figure>
+            <p>{{ project.description }}</p>
+        </div>
     </div>
 </template>
 
