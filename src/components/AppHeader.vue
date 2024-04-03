635cb7b45d1eed6e2529d58b8f187aa97c999752
Polvere243 <danielercoli@gmail.com>
2024-04-03 22:27:35 +0200

    created two components and emptied the style scss

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script>
+import BaseNavbar from './BaseNavbar.vue'
+
+export default {
+    name: 'AppHeader',
+    components: [AppHeader,]
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped></style>
